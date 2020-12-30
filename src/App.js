6d5dfc70493869d707b5bf2fb3142ff4b58719bb
ianlancaster <ianclancaster@gmail.com>
2020-12-30 15:44:00 -0700

    initial sticky header POC

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Col = ({ rowKey, colKey }) => (
+  <div className="col">
+    <p>{rowKey}-{colKey}</p>
+  </div>
+)
+
+const StickyCol = ({ rowKey }) => (
+  <div className="col sticky-col">
+    <p>{rowKey}-S</p>
+  </div>
+)
+
+const Row = ({ rowKey }) => {
+  const cols = Array.from({ length: 12 });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row">
+      <StickyCol rowKey={rowKey} />
+      {cols.map((x, i) => <Col key={i} colKey={i} rowKey={rowKey} />)}
+    </div>
+  )
+}
+
+const StickyRow = ({ height }) => {
+  const cols = Array.from({ length: 12 });
+
+  return (
+    <div className="row sticky-row">
+      {cols.map((x, i) => <Col key={i} colKey={i} rowKey='H'/>)}
+    </div>
+  )
+}
+
+const CornerMask = () => (
+  <div className="col corner-mask" />
+)
+
+const App = () => {
+  const rows = Array.from({ length: 12 });
+
+  return (
+    <div className="app">
+      <div className="scroll-wrapper">
+        <div className="scroll-container">
+          <div className="board">
+            <CornerMask />
+            <StickyRow />
+            {rows.map((x, i) => <Row key={i} rowKey={i} />)}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
