7feb7c8bb3da2c93d0d349af1347063db3fb3e87
ianlancaster <ianclancaster@gmail.com>
2020-12-30 18:11:56 -0700

    final sticky header POC

--- src/App.js
+++ src/App.js
@@ -1,56 +1,45 @@
 import './App.css';
+import classNames from 'classnames'
 
-const Col = ({ rowKey, colKey }) => (
-  <div className="col">
+const Col = ({ rowKey, colKey, cellWidth, sticky, fitContents }) => (
+  <div
+    className={classNames('col', { sticky })}
+    style={{ flex: fitContents ? '1' : `0 0 ${cellWidth}px` }}
+  >
     <p>{rowKey}-{colKey}</p>
   </div>
 )
 
-const StickyCol = ({ rowKey }) => (
-  <div className="col sticky-col">
-    <p>{rowKey}-S</p>
+const Row = ({ rowKey, sticky, fitContents, cellHeight, cellWidth, cols }) => (
+  <div
+    className={classNames('row', { sticky })}
+    style={{ flex: `0 0 ${cellHeight}px` }}
+  >
+    <Col {...{ cellWidth, rowKey, fitContents }} colKey="S" sticky />
+    {cols.map((x, i) => (
+      <Col {...{ cellWidth, rowKey, fitContents }} key={i} colKey={i} />
+    ))}
   </div>
 )
 
-const Row = ({ rowKey }) => {
-  const cols = Array.from({ length: 12 });
-
-  return (
-    <div className="row">
-      <StickyCol rowKey={rowKey} />
-      {cols.map((x, i) => <Col key={i} colKey={i} rowKey={rowKey} />)}
+const ScrollContainer = ({ rows, cellHeight = 100, cellWidth = 150, fitContents }) => (
+  <div className="scroll-container">
+    <div className="board" style={{...(fitContents ? {} : { width: cellWidth * rows[0]?.length })}}>
+      <Row {...{ fitContents, cellHeight, cellWidth }} cols={rows[0]} rowKey="H" sticky/>
+      {rows.map((row, i) => (
+        <Row {...{ fitContents, cellHeight, cellWidth }} cols={row} key={i} rowKey={i}/>
+      ))}
     </div>
-  )
-}
-
-const StickyRow = ({ height }) => {
-  const cols = Array.from({ length: 12 });
-
-  return (
-    <div className="row sticky-row">
-      {cols.map((x, i) => <Col key={i} colKey={i} rowKey='H'/>)}
-    </div>
-  )
-}
-
-const CornerMask = () => (
-  <div className="col corner-mask" />
+  </div>
 )
 
 const App = () => {
-  const rows = Array.from({ length: 12 });
+  const cols = Array.from({ length: 12 });
+  const rows = Array.from({ length: 12 }).map(() => cols);
 
   return (
     <div className="app">
-      <div className="scroll-wrapper">
-        <div className="scroll-container">
-          <div className="board">
-            <CornerMask />
-            <StickyRow />
-            {rows.map((x, i) => <Row key={i} rowKey={i} />)}
-          </div>
-        </div>
-      </div>
+      <ScrollContainer rows={rows} fitContents={false} />
     </div>
   );
 }
